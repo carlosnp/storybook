a7c4867bb06e9a13afae38f80bbdfd37b4019bc0
carlos <machinery.e@gmail.com>
2024-05-02 22:22:49 +0000

    Crear componente del home

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('./home/home.module').then(m=> m.HomeModule),
+            },
+        ]
+    },
+];
